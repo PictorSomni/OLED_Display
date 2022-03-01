8b56b401546fc793519c2c9a0eb6747fb861a6f0
Charles Dessenius <dessenius.charles@gmail.com>
2022-03-01 12:44:08 +0100

    New RPG dices !
    Button A = Random Yes / No
    Button B = D6
    Button C = D100

--- OLED Dices/main.py
+++ OLED Dices/main.py
@@ -7,12 +7,6 @@ import digitalio
 from adafruit_debouncer import Debouncer
 from adafruit_display_text import label
 import adafruit_displayio_sh1107
-import usb_hid
-from adafruit_hid.keyboard import Keyboard
-from keyboard_layout_win_fr import KeyboardLayout
-
-# from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-import adafruit_ducky
 
 displayio.release_displays()
 
@@ -37,17 +31,6 @@ button_c = Debouncer(pin_c) #5
 i2c = board.I2C()
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
 
-## Keyboard
-time.sleep(1)
-keyboard = Keyboard(usb_hid.devices)
-# keyboard_layout = KeyboardLayoutUS(keyboard)
-keyboard_layout = KeyboardLayout(keyboard)
-
-
-## DUCKY
-duck = adafruit_ducky.Ducky("wifi_grabber.txt", keyboard, keyboard_layout)
-# duck = adafruit_ducky.Ducky("youtube.txt", keyboard, keyboard_layout)
-
 ## SH1107 is vertically oriented 64x128
 WIDTH = 128
 HEIGHT = 64
@@ -59,15 +42,15 @@ group = displayio.Group()
 display.show(group)
 
 button_bitmap = displayio.Bitmap(5, 5, 1)
-wifi_bitmap = displayio.OnDiskBitmap("/purple.bmp")
+# wifi_bitmap = displayio.OnDiskBitmap("/purple.bmp")
 
 color_palette = displayio.Palette(1)
 color_palette[0] = 0xFFFFFF  # White
 
 button_sprite = displayio.TileGrid(button_bitmap, pixel_shader=color_palette, x=0, y=-10)
-wifi_sprite = displayio.TileGrid(wifi_bitmap, pixel_shader=wifi_bitmap.pixel_shader, x=0, y= 130)
+# wifi_sprite = displayio.TileGrid(wifi_bitmap, pixel_shader=wifi_bitmap.pixel_shader, x=0, y= 130)
 group.append(button_sprite)
-group.append(wifi_sprite)
+# group.append(wifi_sprite)
 
 text = "LE FUTUR"
 text_area = label.Label(terminalio.FONT, text=text, scale=2, color=0xFFFFFF, x=15, y=28)
@@ -83,26 +66,22 @@ while True:
 
     # Check for button presses & set text
     if button_a.fell:
-        button_sprite.y = 10
-        text_area.scale = 3
-        text_area.x = 28
-        text_area.y = 28
-        text_area.text = "WiFi"
-        # wifi_sprite.y = -21
-
-        result = True
-        while result is not False:
-            result = duck.loop()
-
-    elif button_b.fell:
-        button_sprite.y = 28
+        button_sprite.y = 8
         text_area.scale = 5
         text_area.x = 26
         text_area.y = 28
         text_area.text = f"{random.choice(["OUI", "NON"])}"
+        # wifi_sprite.y = -21
+
+    elif button_b.fell:
+        button_sprite.y = 28
+        text_area.scale = 5
+        text_area.x = 52
+        text_area.y = 28
+        text_area.text = f"{random.randint(1, 6)}"
 
     elif button_c.fell:
-        button_sprite.y = 48
+        button_sprite.y = 47
         text_area.scale = 5
         text_area.x = 26
         text_area.y = 28
